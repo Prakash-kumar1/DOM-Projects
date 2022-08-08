7df6292fc61076e3b6cceb96db84ca90cf717806
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-08 12:08:14 +0530

    c

--- Color-Flipper/Flipper.js
+++ Color-Flipper/Flipper.js
@@ -1,1 +1,21 @@
-const
+const getColor = () => {
+    // Hex code
+
+    const randomNumber = Math.floor(Math.random() * 16777215)
+    // console.log(randomNumber) ;
+
+// For converting  Number  in  Hex-Code 
+const randomCode = "#"  +  randomNumber.toString(16) ;
+console.log(randomNumber , randomCode) ;
+document.body.style.backgroundColor  =  randomCode ;
+}
+
+// Event Call
+document.getElementById("btn").addEventListener("click" , getColor) ;
+
+// Intial Call
+getColor() ;
+
+
+
+
