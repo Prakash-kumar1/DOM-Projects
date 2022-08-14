2611bd16e19e94a0a08e7e3b51b3fef65f239c84
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-14 13:03:37 +0530

    Update Interview.js

--- Mock-Interview/Interview.js
+++ Mock-Interview/Interview.js
@@ -10,7 +10,7 @@ let black = document.getElementsByClassName("black-inside")[0]
 let form = document.getElementById("form") ;
 let arr = Array.from(edit.classList) ;
 let done = document.querySelector(".submit-me") ;
-let display = document.getElementById("disp")
+let display = document.getElementById("disp") ;
 let completeds = document.getElementById("completed")
 
 
