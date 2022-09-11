eff82c7ef8cf32711d4d8f498cfe320f41e9b555
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-11 18:29:26 +0530

    <header>

--- Code-Along/script.js
+++ Code-Along/script.js
@@ -1,8 +1,8 @@
 (async()=>{
-    let response = await fetch(``);
+    let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=1`);
     let data = await response.json() ;
     data = data.results ;
     data.map((oneData)=>{
-        
+
     })
 })
