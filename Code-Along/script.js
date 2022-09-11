c0d6cde27ab8fa3691ff7a942c490bdff3e54cf4
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-11 19:31:09 +0530

    akash Kumar 2022

--- Code-Along/script.js
+++ Code-Along/script.js
@@ -1,13 +1,30 @@
+var cart = 0 ;
+
 (async()=>{
     let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=1`);
     let data = await response.json() ;
+    console.log(data)
     data = data.results ;
     data.map((oneData)=>{
         let main = document.querySelector('main');
-        main.innerHTML += <div className="movie">
-            <img src="https://image.tmdb.org/t/p/original/${oneData.poster_path}"></img>
+        main.innerHTML += `<div class="movie">
+            <img src="https://image.tmdb.org/t/p/original/${oneData.poster_path}">
+            <div class="head-er" >
             <h2>${oneData.title}</h2>
-        </div>
+            <button class="addButton" onClick="add_to_cart(event)" id = ${oneData.id}>Add+</button>
+            </div>   
+        </div>` ;
 
     })
-})
+})() ;
+
+function add_to_cart(e){
+   let idd = e.target.id ;
+   for(let i=0 ; i<data.length ; i++){
+    if(data[i].id === idd){
+        cart.push
+
+    }
+   }
+
+}
