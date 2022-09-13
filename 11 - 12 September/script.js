ecaf01d5e5e7f2105a37212f856a5ad49a19a4db
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-13 16:40:39 +0530

    movie

--- 11 - 12 September/script.js
+++ 11 - 12 September/script.js
@@ -2,6 +2,7 @@ var cart = 0 ;
 
 (async()=>{
     let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=9`);
+    console.log(response) ;
     let data = await response.json() ;
     console.log(data)
     data = data.results ;
