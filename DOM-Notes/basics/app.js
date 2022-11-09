ab5eff70c1898527c3a846c463c2d975f9e49fd3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-09 21:47:23 +0530

    DOM  Basic  Methods  and Concepts

---
+++ DOM-Notes/basics/app.js
@@ -0,0 +1,31 @@
+// GetElementById()
+const title = document.getElementById("main-heading") ;
+console.log(title) ;
+
+
+// GetElementByClassName()
+let listItems = document.getElementsByClassName('list-items') ;
+console.log(listItems) ;
+
+
+// GetElementByTagName()
+ listItems = document.getElementsByTagName('li') ;
+console.log(listItems) ;
+
+
+// Query selector method
+let container = document.querySelector('div') ;
+console.log(container) ;
+
+ container = document.querySelector('ul') ;
+console.log(container) ;
+
+
+//  Query selector All method
+ let store = document.querySelectorAll('div') ;
+ console.log(store) ;
+
+ store = document.querySelectorAll('ul') ;
+ console.log(store) ;
+
+
