fffeba620c92fa328eae5a9713c68e4aededbbe8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-11 10:29:50 +0530

    Basic DOM Project

--- DOM-Notes/project-1/app.js
+++ DOM-Notes/project-1/app.js
@@ -1,7 +1,23 @@
+var form = document.getElementById('addForm') ;
+var itemList = document.querySelector('#items') ;
 
+// Form submit event 
+form.addEventListener('submit' , addItem) ;
 
+// Add item 
+function addItem(e){
+    e.preventDefault() ;
 
+// Get Input value 
+var newItem = document.getElementById('item') ;
 
+// create new Li Element
+let li = document.createElement('li') ;
+// Add Class
+li.className = 'list-group-item' ;
+console.log(li) ;
+
+}
 
 
 
